cb9ed425f4f7583da61f7ec7f9e3ac4184a611c7
massegu <massegu@gmail.com>
2025-10-06 15:24:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -185,7 +185,7 @@ elif visualizacion == "Animación paso a paso":
                 alpha = abs(valor) / 6
                 rect = plt.Rectangle((col + j, fila + i), 1, 1, color=color, alpha=alpha)
                 ax.add_patch(rect)
-            ax.text(col + j + 0.5, fila + i + 0.5, f"{valor:.0f}", ha='center', va='center', fontsize=6, color='white')
+            #ax.text(col + j + 0.5, fila + i + 0.5, f"{valor:.0f}", ha='center', va='center', fontsize=6, color='white')
 
     # Borde azul y etiquetas
     ax.add_patch(plt.Rectangle((col, fila), 5, 5, fill=False, edgecolor='blue', linewidth=2))
