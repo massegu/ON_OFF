2da55ed7ccbdd68cd39b78cea122854bd2405561
massegu <massegu@gmail.com>
2025-10-08 23:47:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -241,11 +241,11 @@ elif visualizacion == "Solo Bipolares":
     <b>🧠 Procesamiento bipolar:</b><br>
     Las células bipolares responden de forma proporcional a la luminancia local, sin antagonismo espacial. Esta visualización muestra cómo se codifica la información visual si solo se procesara a nivel bipolar, sin la modulación centro ON / centro OFF de las ganglionares.<br><br>
     🔹 <b>Resultado:</b> Las células bipolares responden de forma proporcional a la luminancia local, es decir, transmiten la cantidad de luz que incide en cada punto de la retina sin realizar comparaciones con regiones vecinas.<b> En cambio, las células ganglionares introducen antagonismo espacial, una propiedad clave que permite detectar contrastes y bordes.</b> 
-    <b>Este antagonismo se basa en comparar la luz que llega al centro del campo receptivo con la que llega a la periferia:</b>
-        Si el centro está iluminado y la periferia oscura (Centro ON), la célula se activa. </b> <b> Si el centro está oscuro y la periferia iluminada (Centro OFF), también se activa, pero con polaridad inversa.</b><b>Este mecanismo de antagonismo espacial no está presente en las bipolares, por lo que su respuesta es más difusa y menos selectiva.</b>
-    <b>🔍 ¿Qué aporta el antagonismo espacial?</b> <b>Permite detectar bordes, contornos y transiciones de luminancia.</b>
-    <b>Mejora la eficiencia del sistema visual, reduciendo redundancia.</b>
-    <b>Facilita la codificación de formas y objetos, incluso en condiciones de iluminación variable.</b>
+    <b>Este antagonismo se basa en comparar la luz que llega al centro del campo receptivo con la que llega a la periferia:</b><br>
+        Si el centro está iluminado y la periferia oscura (Centro ON), la célula se activa. </b> <br><b> Si el centro está oscuro y la periferia iluminada (Centro OFF), también se activa, pero con polaridad inversa.</b><br><b>Este mecanismo de antagonismo espacial no está presente en las bipolares, por lo que su respuesta es más difusa y menos selectiva.</b><br>
+    <b>🔍 ¿Qué aporta el antagonismo espacial?</b> <b>Permite detectar bordes, contornos y transiciones de luminancia.</b><br>
+    <b>Mejora la eficiencia del sistema visual, reduciendo redundancia.</b><br>
+    <b>Facilita la codificación de formas y objetos, incluso en condiciones de iluminación variable.</b><br>
     <b>En esta visualización, al mostrar solo el procesamiento bipolar, se observa una imagen suavizada, sin realce de bordes.</b><b> Esto ilustra cómo las ganglionares enriquecen la percepción visual al añadir contraste espacial.</b>
     </div>
     """, unsafe_allow_html=True)
